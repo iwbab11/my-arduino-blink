4a674b398d501a5fccf58480b7e877dbd5105339
Katya <katya_be2008@mail.ru>
2026-02-12 18:48:29 +0300

    feat: add second LED on pin 12, alternate blinking

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -1,9 +1,12 @@
 void setup() {
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 void loop() {
-  digitalWrite(9, HIGH);   
-  delay(500);                      
-  digitalWrite(9, LOW);   
-  delay(500);                     
+  digitalWrite(9, HIGH);  
+  digitalWrite(10, HIGH); 
+  delay(100);                      
+  digitalWrite(9, LOW);  
+  digitalWrite(10, LOW); 
+  delay(100);                     
 }
