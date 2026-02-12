3200603a76ba5e7f92a93fd34b7536bc43d242ec
Katya <katya_be2008@mail.ru>
2026-02-12 19:03:10 +0300

    add blinkSOS function

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -1,12 +1,15 @@
 void setup() {
   pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+void blinkSOS(){
+  print("...---...");
 }
 void loop() {
   digitalWrite(9, HIGH);  
-  digitalWrite(10, HIGH); 
+  digitalWrite(12, HIGH); 
   delay(100);                      
   digitalWrite(9, LOW);  
-  digitalWrite(10, LOW); 
+  digitalWrite(12, LOW); 
   delay(100);                     
 }
