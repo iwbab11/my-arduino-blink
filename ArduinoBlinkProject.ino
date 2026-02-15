2a9ac9a08d0df6a2d62be77ecd138323e50c3475
Katya <katya_be2008@mail.ru>
2026-02-16 01:18:05 +0300

    feat: add blinkSOS function

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -6,5 +6,26 @@ void loop() {
   digitalWrite(9, HIGH);  
   delay(500);                      
   digitalWrite(9, LOW);  
-  delay(500);   
+  delay(500);
+  blinkSOS();
+}
+void blinkSOS(){
+  for(int i=0;i<2;i++){
+    digitalWrite(10, HIGH);
+    delay(500);
+    digitalWrite(10, LOW);
+    delay(250);
+  }
+  for(int i=0;i<2;i++){
+    digitalWrite(10, HIGH);
+    delay(3000);
+    digitalWrite(10, LOW);
+    delay(1000);
+  }
+  for(int i=0;i<2;i++){
+    digitalWrite(10, HIGH);
+    delay(500);
+    digitalWrite(10, LOW);
+    delay(250);
+  }              
 }
